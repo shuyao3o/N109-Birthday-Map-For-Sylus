5dc2aae066ce66bcce76700568795218770cb001
shuyao3o <Jasmine_e2020@163.com>
2026-04-16 22:40:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -194,12 +194,15 @@ except Exception as e:
 @st.cache_data(ttl=300)
 def fetch_data():
     try:
-        # 拉取 5000 条作为样本池，保证全球各地的猎人都能被抽到
+        # 拉取 5000 条作为样本池
         response = supabase.table("blessings").select("*").order("id", desc=True).limit(5000).execute()
         data = response.data
         return data
     except Exception as e:
+        # 🦅 强制让隐形报错显形！
+        st.error(f"⚠️ 雷达读取失败，真实原因: {e}")
         return []
+
 
 def save_data(name, city, lon, lat, message):
     try:
